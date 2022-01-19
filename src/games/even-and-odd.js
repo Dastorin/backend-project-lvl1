6cdb67c8044b5eb6a52afb7fab9204a3d7459ae0
Dastorin <kfp.toxa@gmail.com>
2022-01-20 01:25:10 +1200

    fix even-and-odd.js

--- src/games/even-and-odd.js
+++ src/games/even-and-odd.js
@@ -1,7 +1,9 @@
+const isEven = (number) => ((number % 2 === 0) ? 'yes' : 'no');
+
 const definitionEvenOrOdd = () => {
   const randomNum = Math.floor(Math.random() * 100);
   const question = `Question: ${randomNum}`;
-  const corAnswer = (randomNum % 2 === 0) ? 'yes' : 'no';
+  const corAnswer = isEven(randomNum);
   return [corAnswer, question];
 };
 
