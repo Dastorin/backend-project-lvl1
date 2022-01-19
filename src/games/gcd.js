d780264beaf8bf8a3ec8b9bf17f30e1f4e74270e
Dastorin <kfp.toxa@gmail.com>
2022-01-20 01:01:48 +1200

    fix gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,16 +1,13 @@
-import readlineSync from 'readline-sync';
-
-const gcd = () => {
-  const num1 = Math.floor(Math.random() * 20) + 1;// рандомное определение первого числа
-  const num2 = Math.floor(Math.random() * 20) + 1;// рандомное определение второго числа
-  console.log(`Question: ${num1} ${num2}`);
+const expressinGcd = () => {
+  const num1 = Math.floor(Math.random() * 20) + 1;
+  const num2 = Math.floor(Math.random() * 20) + 1;
+  const question = `Question: ${num1} ${num2}`;
   let result = 0;
   for (let index = 1; index <= Math.min(num1, num2); index += 1) {
     if (num1 % index === 0 && num2 % index === 0) {
       result = index;
     }
   }
-  const answer = readlineSync.question('Your answer: '); // запрос ответа от пользователя;
-  return [result, Number(answer)];
+  return [String(result), question];
 };
-export default gcd;
+export default expressinGcd;
