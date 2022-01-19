0aba8e4bf150fde229a77505d595c21bd7f39af4
Dastorin <kfp.toxa@gmail.com>
2022-01-20 01:30:24 +1200

    fix gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,13 +1,18 @@
-const expressinGcd = () => {
-  const num1 = Math.floor(Math.random() * 20) + 1;
-  const num2 = Math.floor(Math.random() * 20) + 1;
-  const question = `Question: ${num1} ${num2}`;
+const isGcd = (num1, num2) => {
   let result = 0;
   for (let index = 1; index <= Math.min(num1, num2); index += 1) {
     if (num1 % index === 0 && num2 % index === 0) {
       result = index;
     }
   }
-  return [String(result), question];
+  return result;
 };
-export default expressinGcd;
+
+const gcd = () => {
+  const num1 = Math.floor(Math.random() * 20) + 1;
+  const num2 = Math.floor(Math.random() * 20) + 1;
+  const question = `Question: ${num1} ${num2}`;
+  const corAnswer = isGcd(num1, num2);
+  return [String(corAnswer), question];
+};
+export default gcd;
