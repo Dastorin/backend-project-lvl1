d783829284c64f1d71f444d9bf1339a21e107538
Dastorin <kfp.toxa@gmail.com>
2022-01-20 01:02:23 +1200

    fix progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,22 +1,18 @@
-import readlineSync from 'readline-sync';
-
 const progression = () => {
-  let num = Math.floor(Math.random() * 100);// определение значения первого элемента ряда
-  const count = Math.floor(Math.random() * 5) + 5;// определение количество элеметнов ряда
-  const step = Math.floor(Math.random() * 20) + 1;// определение шага прогрессии
-  const shadow = Math.floor(Math.random() * count);// определение номера скрытого элемента
-  const result = [];// объявлнеие массива для арифметической прогресси
+  let num = Math.floor(Math.random() * 100);
+  const countElements = Math.floor(Math.random() * 5) + 5;
+  const stepProgression = Math.floor(Math.random() * 20) + 1;
+  const shadowElement = Math.floor(Math.random() * countElements);
+  const result = [];
   let i = 0;
-  // определение стека арифмитечскеой прогресси
   do {
     result.push(num);
-    num += step;
+    num += stepProgression;
     i += 1;
-  } while (i < count);
-  const corAnswer = result[shadow];// сохранение значения правильного ответа
-  result[shadow] = '..';// скрытие элемета который необходимо найти
-  console.log(`Question: ${result.join(' ')}`);// вывод на экран вопроса
-  const answer = readlineSync.question('Your answer: '); // запрос ответа от пользователя;
-  return [corAnswer, Number(answer)];
+  } while (i < countElements);
+  const corAnswer = result[shadowElement];
+  result[shadowElement] = '..';
+  const question = `Question: ${result.join(' ')}`;
+  return [String(corAnswer), question];
 };
 export default progression;
