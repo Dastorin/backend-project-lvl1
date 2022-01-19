7477c03986a4bcf168d177a9039520c2924e6557
Dastorin <kfp.toxa@gmail.com>
2022-01-20 01:46:24 +1200

    fix progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,18 +1,24 @@
-const progression = () => {
-  let num = Math.floor(Math.random() * 100);
-  const countElements = Math.floor(Math.random() * 5) + 5;
-  const stepProgression = Math.floor(Math.random() * 20) + 1;
-  const shadowElement = Math.floor(Math.random() * countElements);
+const generationProgression = (startNum, count, step) => {
   const result = [];
+  let num = startNum;
   let i = 0;
   do {
     result.push(num);
-    num += stepProgression;
+    num += step;
     i += 1;
-  } while (i < countElements);
-  const corAnswer = result[shadowElement];
-  result[shadowElement] = '..';
-  const question = `Question: ${result.join(' ')}`;
+  } while (i < count);
+  return result;
+};
+
+const progression = () => {
+  const startNum = Math.floor(Math.random() * 100);
+  const countElements = Math.floor(Math.random() * 5) + 5;
+  const stepProgression = Math.floor(Math.random() * 20) + 1;
+  const shadowElement = Math.floor(Math.random() * countElements);
+  const arithmeticProgres = generationProgression(startNum, countElements, stepProgression);
+  const corAnswer = arithmeticProgres[shadowElement];
+  arithmeticProgres[shadowElement] = '..';
+  const question = `Question: ${arithmeticProgres.join(' ')}`;
   return [String(corAnswer), question];
 };
 export default progression;
