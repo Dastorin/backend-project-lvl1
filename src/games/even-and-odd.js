9f5d5b5efbc7b78c7f0cc0428df92bf63e0b3b54
Dastorin <kfp.toxa@gmail.com>
2022-01-20 01:01:35 +1200

    fix even-and-odd.js

--- src/games/even-and-odd.js
+++ src/games/even-and-odd.js
@@ -1,15 +1,8 @@
-import readlineSync from 'readline-sync';
-
-const evenAndOdd = () => { // функция чет/нечет
-  // определение рандомного целого положительного числа от 0 до 100.
+const definitionEvenOrOdd = () => {
   const randomNum = Math.floor(Math.random() * 100);
-  console.log(`Question: ${randomNum}`); // вывод на экран вороса с рандомным числом.
-  const answer = readlineSync.question('Your answer: '); // запрос ответа от пользователя;
-  // поределение правильного овета
-  if (randomNum % 2 === 0) {
-    return ['yes', answer]; // функция возвращает значение yes и ответ пользователя
-  }
-  return ['no', answer];// функция возвращает знаечение no и ответ пользователя
+  const question = `Question: ${randomNum}`;
+  const corAnswer = (randomNum % 2 === 0) ? 'yes' : 'no';
+  return [corAnswer, question];
 };
 
-export default evenAndOdd;
+export default definitionEvenOrOdd;
