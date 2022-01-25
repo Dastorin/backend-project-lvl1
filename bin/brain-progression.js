63f8850aa0f0e1d1d37468cadd4c8d12b443a012
Dastorin <kfp.toxa@gmail.com>
2022-01-25 15:42:19 +1200

    update brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-import games from '../src/index.js';
 import progression from '../src/games/progression.js';
 
-const rules = 'What number is missing in the progression?';
-games(rules, progression);
+progression();
