3425663bc1dae01c83291a40f802de330989dbe4
Dastorin <kfp.toxa@gmail.com>
2022-01-25 15:14:19 +1200

    update brain-prime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,6 +1,3 @@
-#!/usr/bin/env node
-import games from '../src/index.js';
 import prime from '../src/games/prime.js';
 
-const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-games(rules, prime);
+prime();
