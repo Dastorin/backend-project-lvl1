0acaffa9f11afef1543cd7fe4c5eeb6e54992bcf
Dastorin <kfp.toxa@gmail.com>
2022-01-25 20:29:53 +1200

    fix random.js

--- src/games/random.js
+++ src/games/random.js
@@ -1,5 +1,5 @@
-const getRundomNumber = (ending, start = 0) => {
-  const result = Math.floor(Math.random() * ending + start);
+const getRundomNumber = (end, begin = 0) => {
+  const result = Math.floor(Math.random() * end + begin);
   return result;
 };
 export default getRundomNumber;
