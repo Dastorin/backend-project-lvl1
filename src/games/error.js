aa29d5fb32e16200529709c50430c568c9a693c5
Dastorin <kfp.toxa@gmail.com>
2022-01-25 16:32:09 +1200

    remove error.js

--- src/games/error.js
+++
@@ -1,6 +0,0 @@
-// функция в случае неверного ответа
-const error = (answer, corAnswer, name) => {
-  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${corAnswer}'.`);
-  console.log(`Let's try again, ${name}!`);
-};
-export default error;
