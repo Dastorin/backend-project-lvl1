d2774cdb4e4c291d02991a5f4fd09e22e66fbdbc
Dastorin <kfp.toxa@gmail.com>
2022-01-25 14:23:22 +1200

    update brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
 import calc from '../src/games/calc.js';
-import games from '../src/index.js';
 
-const rules = 'What is the result of the expression?';
-games(rules, calc);
+calc();
