c238c222ebee5746e7bc06b0dd670e5c2c7271f6
Dastorin <kfp.toxa@gmail.com>
2022-01-25 16:03:46 +1200

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import readlineSync from 'readline-sync';
 import error from './games/error.js';
 
+const winsCount = 3;
+
 const games = (rules, game) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log(rules);
-  const winsCount = 3;
   for (let i = 0; i < winsCount; i += 1) {
     const [corAnswer, question] = game();
     console.log(question);
