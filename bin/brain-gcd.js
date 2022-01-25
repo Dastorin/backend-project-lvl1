32659f128dc0332ce4c9baa344af1bd105955c56
Dastorin <kfp.toxa@gmail.com>
2022-01-25 15:02:46 +1200

    update brain-gcd.js

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
 import gcd from '../src/games/gcd.js';
-import games from '../src/index.js';
 
-const rules = 'Find the greatest common divisor of given numbers.';
-games(rules, gcd);
+gcd();
