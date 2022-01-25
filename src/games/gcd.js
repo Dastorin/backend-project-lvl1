1e9218c716780fadd1b7d4236e3169736ad58bc7
Dastorin <kfp.toxa@gmail.com>
2022-01-25 15:02:11 +1200

    fix gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,18 +1,23 @@
-const isGcd = (num1, num2) => {
+import getRundomNumber from './random.js';
+import games from '../index.js';
+
+const isGcd = (number1, number2) => {
   let result = 0;
-  for (let index = 1; index <= Math.min(num1, num2); index += 1) {
-    if (num1 % index === 0 && num2 % index === 0) {
+  for (let index = 1; index <= Math.min(number1, number2); index += 1) {
+    if (number1 % index === 0 && number2 % index === 0) {
       result = index;
     }
   }
   return result;
 };
 
+const rules = 'Find the greatest common divisor of given numbers.';
+
 const gcd = () => {
-  const num1 = Math.floor(Math.random() * 20) + 1;
-  const num2 = Math.floor(Math.random() * 20) + 1;
-  const question = `Question: ${num1} ${num2}`;
-  const corAnswer = isGcd(num1, num2);
-  return [String(corAnswer), question];
+  const randomNumber1 = getRundomNumber(20, 1);
+  const randomNumber2 = getRundomNumber(20, 1);
+  const question = `Question: ${randomNumber1} ${randomNumber2}`;
+  const correctAnswer = isGcd(randomNumber1, randomNumber2);
+  return [String(correctAnswer), question];
 };
-export default gcd;
+export default () => games(rules, gcd);
