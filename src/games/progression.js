7d4c95bbfb9c9384dfc85683876407a975ed356c
Dastorin <kfp.toxa@gmail.com>
2022-01-30 22:41:14 +1200

    fix check #5 remark #4

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,14 +3,13 @@ import identificationWinOrLoose from '../index.js';
 
 const rules = 'What number is missing in the progression?';
 
-const getProgression = (start, count, step, hiddenIndex) => {
+const generateQuestion = (start, count, step, hiddenIndex) => {
   const question = [];
   for (let index = 0; index < count; index += 1) {
     question.push(start + (step * index));
   }
-  const correctAnswer = question[hiddenIndex];
   question[hiddenIndex] = '..';
-  return [String(correctAnswer), question];
+  return question;
 };
 
 const generateProgression = () => {
@@ -18,7 +17,8 @@ const generateProgression = () => {
   const length = getRundomNumber(5, 10);
   const step = getRundomNumber(1, 20);
   const hiddenIndex = getRundomNumber(0, length - 1);
-  return getProgression(start, length, step, hiddenIndex);
+  const correctAnswer = start + step * (hiddenIndex);
+  return [String(correctAnswer), generateQuestion(start, length, step, hiddenIndex)];
 };
 
 export default () => identificationWinOrLoose(rules, generateProgression);
