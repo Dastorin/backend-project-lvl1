0f8459eb657b9db014eab0e4478a38b1a90a55e7
Dastorin <kfp.toxa@gmail.com>
2022-01-30 22:14:08 +1200

    fix check #5 remark #2

--- src/games/even-and-odd.js
+++ src/games/even-and-odd.js
@@ -6,9 +6,8 @@ const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 const isEven = (number) => (number % 2 === 0);
 
 const definitionEvenOrOdd = () => {
-  const randomNumber = getRundomNumber(1, 100);
-  const question = randomNumber;
-  const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
+  const question = getRundomNumber(1, 100);
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
   return [correctAnswer, question];
 };
 
